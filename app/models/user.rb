33569cd6c3e05923679d336f439c88b2f71a363d
Spike Vinz Cruz <spikevinz@gmail.com>
2021-03-15 08:37:14 +0800

    adding user/new form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, unique:true, length: { in: 4..15 }
-  validates :email, email: true, presence: true, unique:true
+  validates :username, presence: true, length: { in: 4..15 }
+  validates :email, presence: true
   validates :password, presence: true, length: { in: 8..20 }
 end
