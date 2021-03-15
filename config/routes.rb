a5748fa5f11096ef5c08d623d0d3e85ed881a8ef
Spike Vinz Cruz <spikevinz@gmail.com>
2021-03-15 09:59:38 +0800

    fixing user#edit and user#create, and adding error messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :edit]
+  resources :users, only: [:new, :create, :edit, :update]
+  patch 'users/:id', to: 'users#update'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
